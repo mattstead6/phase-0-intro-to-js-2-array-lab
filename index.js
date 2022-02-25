ee08a5b4f53a758c0621244d3bd19141edfbee35
Matthew Stead <mattstead6@gmail.com>
2022-02-24 22:23:52 -0500

    commit tree 01f9b0794470bb857cedf2b32474a524f22c551b

--- index.js
+++ index.js
@@ -1,35 +1,80 @@
-// Write your solution here!
 const cats = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendCat (name){
-    return cats.push(name)
-}
+// function destructivelyAppendCat(name){
+// return cats.push(name)};
 
-function destructivelyPrependCat (name){
-    return cats.unshift(name)
-}
 
-function destructivelyRemoveLastCat (){
-    return cats.pop();
-}
 
-function destructivelyRemoveFirstCat(){
-    return cats.shift();
-}
+const destructivelyAppendCat = name => cats.push(name);
 
-function appendCat (name){
-   return [...cats, name];
-}
+const destructivelyPrependCat = name => cats.unshift(name);
 
-function prependCat (name){
-    return [name, ...cats];
-}
+const destructivelyRemoveLastCat = () => cats.pop();
 
-function removeLastCat(){
-    return cats.slice(0,-1);
-}
+const destructivelyRemoveFirstCat = () => cats.shift();
 
-function removeFirstCat(){
-    return cats.slice(1);
-}
+const appendCat = name => [...cats, name];
+
+const prependCat = name => [name, ...cats];
+
+const removeLastCat = () => [...cats.slice(0, (cats.length-1))];
+
+const removeFirstCat = () => [...cats.slice(1)];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const cats = ["Milo", "Otis", "Garfield"];
+
+// function destructivelyAppendCat (name){
+//     return cats.push(name)
+// }
+
+// function destructivelyPrependCat (name){
+//     return cats.unshift(name)
+// }
+
+// function destructivelyRemoveLastCat (){
+//     return cats.pop();
+// }
+
+// function destructivelyRemoveFirstCat(){
+//     return cats.shift();
+// }
+
+// function appendCat (name){
+//    return [...cats, name];
+// }
+
+// function prependCat (name){
+//     return [name, ...cats];
+// }
+
+// function removeLastCat(){
+//     return cats.slice(0,-1);
+// }
+
+// function removeFirstCat(){
+//     return cats.slice(1);
+// }
 
